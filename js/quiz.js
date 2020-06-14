3f6188f6843408fe3c473c6eb719bd65f8b59721
Nero2005 <59165938+Nero2005@users.noreply.github.com>
2020-06-14 20:16:16 +0100

    Add files via upload

--- js/quiz.js
+++ js/quiz.js
@@ -1,6 +1,61 @@
 var total = 0;
 
 
+function clear_select(optname){
+    var option = document.getElementsByName(optname);
+    for(let opt of option){
+        opt.checked = false;
+    }
+}
+
+function check_mark(containerid){
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("width", "50");
+    svg.setAttribute("height", "50");
+    var re1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    var re2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    re1.setAttribute("stroke", "green");
+    re2.setAttribute("stroke", "green");
+    re1.setAttribute("x1", 25);
+    re1.setAttribute("y1", 25);
+    re1.setAttribute("x2", 30);
+    re1.setAttribute("y2", 30);
+    re2.setAttribute("x1", 30);
+    re2.setAttribute("y1", 30);
+    re2.setAttribute("x2", 40);
+    re2.setAttribute("y2", 20);
+    svg.appendChild(re1);
+    svg.appendChild(re2);
+    var container = document.getElementById(containerid);
+    console.log(svg, re1, re2);
+    container.appendChild(svg);
+    svg.style.alignSelf = "left";
+}
+
+
+function cross_mark(containerid){
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("width", "25");
+    svg.setAttribute("height", "25");
+    var re1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    var re2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    re1.setAttribute("stroke", "red");
+    re2.setAttribute("stroke", "red");
+    re1.setAttribute("x1", 20);
+    re1.setAttribute("y1", 20);
+    re1.setAttribute("x2", 30);
+    re1.setAttribute("y2", 30);
+    re2.setAttribute("x1", 10);
+    re2.setAttribute("y1", 10);
+    re2.setAttribute("x2", 30);
+    re2.setAttribute("y2", 30);
+    svg.appendChild(re1);
+    svg.appendChild(re2);
+    var container = document.getElementById(containerid);
+    container.appendChild(svg);
+}
+
+
 function result(correct, q_options){
     /*Gets the questions in the quiz, the var required for the respective list of options
     and the list of the correct ids
@@ -14,11 +69,24 @@ function result(correct, q_options){
             if (j.checked){
                 if (j.id == correct[i]){
                     window.total += 1;
+                    var inc = document.getElementById(j.id);
+                    inc.parentElement.style.backgroundColor = "rgb(50, 200, 0)";
+                    var par = inc.parentElement.parentElement;
+                    var ch = par.children[1];
+                    var ques = par.children[0];
+                    ques.style.color = "rgb(50, 200, 0)";
+                    ch.innerHTML = "5/5";
                 }
                 else {
-                    console.log(correct[i]);
                     var inc = document.getElementById(correct[i]);
                     inc.parentElement.style.backgroundColor = "rgb(50, 200, 0)";
+                    var cor = document.getElementById(j.id);
+                    cor.parentElement.style.backgroundColor = "rgb(200, 50, 0)";
+                    var par = inc.parentElement.parentElement;
+                    var ch = par.children[1];
+                    var ques = par.children[0];
+                    ques.style.color = "rgb(200, 50, 0)";
+                    ch.innerHTML = "0/5";
                 }
             }
         }
